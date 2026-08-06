520aae85db8eb4c82c783b4df56830cc4d75bb1d
Wouter Schoenmakers <wouter@eldarmail.nl>
2026-08-06 12:16:32 +0200

    Started on core block and axi interface, need to finish

---
+++ swing/hdl/S_AXIS_CORE.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/06/2026 09:23:40 AM
+// Design Name: 
+// Module Name: S_AXIS_CORE
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module S_AXIS_CORE(
+
+    );
+endmodule
---
+++ swing/hdl/core.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/06/2026 09:23:40 AM
+// Design Name: 
+// Module Name: core
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module core(
+
+    );
+endmodule
